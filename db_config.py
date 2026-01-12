965c9e9e91b91ec2118eea0bf167e839c10f30ab
Paul Andrei De Guzman <deguzmanpaulandrei13@gmail.com>
2026-01-12 22:19:56 +0800

    Connect Railway MySQL and deploy

--- db_config.py
+++ db_config.py
@@ -1,18 +1,21 @@
+import os
 import mysql.connector
 from mysql.connector import Error
 
 def create_connection():
     try:
         connection = mysql.connector.connect(
-            host="127.0.0.1",
-            user="root",
-            password="",
-            database="hotel_db",
-            port=3306
+            host=os.getenv("DB_HOST"),
+            user=os.getenv("DB_USER"),
+            password=os.getenv("DB_PASSWORD"),
+            database=os.getenv("DB_NAME"),
+            port=int(os.getenv("DB_PORT"))  
         )
+
         if connection.is_connected():
             print("✅ Connected to MySQL successfully!")
             return connection
+
     except Error as e:
         print(f"❌ Database Error: {e}")
         return None
